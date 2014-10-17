08748cfa785c56b22d82085a6ffe6231fc1497f7
Kit Cambridge <github@kitcambridge.be>
2014-10-16 23:36:04 -0700

    `TestNotify`: Use random device and channel IDs.
    The server now ensures that device and channel IDs included in `hello`
    and `register` messages are valid UUIDs. Prefixed IDs are no longer
    supported.

--- tests/test_notify.py
+++ tests/test_notify.py
@@ -9,7 +9,7 @@ import unittest
 import websocket
 
 from pushtest.pushTestCase import PushTestCase
-from utils import (get_uaid, send_http_put)
+from pushtest.utils import (get_uaid, send_http_put)
 
 
 class TestNotify(PushTestCase):
@@ -19,9 +19,11 @@ class TestNotify(PushTestCase):
             websocket.enableTrace(True)
 
     def test_two_chan(self):
+        self.uaid = get_uaid()
+        self.chan1_id = get_uaid()
         self.chan1 = ""
+        self.chan2_id = get_uaid()
         self.chan2 = ""
-        self.uaid = get_uaid('notify')
 
         def on_close(ws):
             self.log('on_close:')
@@ -34,9 +36,9 @@ class TestNotify(PushTestCase):
 
         def _check_updates(updates_list, chan1_val="", chan2_val=""):
             for chan in updates_list:
-                if chan1_val != "" and chan["channelID"] == "tn.chan1":
+                if chan1_val != "" and chan["channelID"] == self.chan1_id:
                     _assert_equal(chan["version"], chan1_val)
-                if chan2_val != "" and chan["channelID"] == "tn.chan2":
+                if chan2_val != "" and chan["channelID"] == self.chan2_id:
                     _assert_equal(chan["version"], chan2_val)
 
             return
@@ -68,7 +70,7 @@ class TestNotify(PushTestCase):
             elif ws.state == 'update1':
                 #verify chan1
                 self.compare_dict(ret, {"messageType": "notification"}, True)
-                _assert_equal(ret["updates"][0]["channelID"], "tn_chan1")
+                _assert_equal(ret["updates"][0]["channelID"], self.chan1_id)
                 _check_updates(ret["updates"], 12345789)
 
                 # http put to chan2
@@ -93,7 +95,7 @@ class TestNotify(PushTestCase):
                 # update the same channel
                 self.compare_dict(ret, {"messageType": "notification"}, True)
                 for chan in ret["updates"]:
-                    if chan["channelID"] == "tn.chan2":
+                    if chan["channelID"] == self.chan2_id:
                         # check if 0 version returns epoch
                         _assert_equal(len(str(chan["version"])), 10)
 
@@ -106,7 +108,7 @@ class TestNotify(PushTestCase):
                 # update the same channel
                 self.compare_dict(ret, {"messageType": "notification"}, True)
                 for chan in ret["updates"]:
-                    if chan["channelID"] == "tn.chan1":
+                    if chan["channelID"] == self.chan1_id:
                         # check if 0 version returns epoch
                         _assert_equal(len(str(chan["version"])), 10)
 
